1f249bc86e263c5cb04dd7033dcc1edaa3e3ce97
Candidate 1000031 <Candidate1000031@users.noreply.github.com>
2015-12-30 21:46:54 +0100

    100 runs and fresh set of paths for pricing

---
+++ section_1/amer_mc_modified.m
@@ -0,0 +1,43 @@
+%
+% Based on code originally downloaded from 
+% http://www.mathworks.com/matlabcentral/fileexchange/
+%        16476-pricing-american-options/content/AmericanOptLSM.m
+%
+% AmericanPutLSM - Price an american put option via Longstaff-Schwartz Method
+%
+% Inputs:
+%
+%   S0      Initial asset price
+%   K       Strike Price
+%   r       Interest rate
+%   T       Time to maturity of option
+%   sigma   Volatility of underlying asset
+%   N       Number of timesteps
+%   M       Number of paths 
+
+S0  = 1.0;   % initial asset price
+K   = 1.0;   % strike
+r   = 0.05;  % risk-free interest rate
+T   = 1.0;   % time to maturity
+sig = 0.2;   % volatility
+
+N   = 64;    % number of timesteps
+M   = 1e5;   % number of paths
+
+R   = 100;   % number of runs
+
+dt = T/N;
+
+fprintf(' Longstaff-Schwartz Monte Carlo method \n');
+P = zeros(R, 1);
+for i = 1:1:R
+    fprintf('\nRun No. %d:\n', i);
+    P(i) = single_run(N, M, dt, S0, r, sig, K);
+end
+
+val = mean(P);
+std_err = std(P) / sqrt(length(P)); % standard error of the mean
+
+fprintf('Average of %d runs: %f\n', R, val);
+fprintf('Confidence interval (2 standard errors of mean): [%f, %f]\n', ...
+        val - 2 * std_err, val + 2 * std_err);
---
+++ section_1/single_run.m
@@ -0,0 +1,61 @@
+function val = single_run(N, M, dt, S0, r, sig, K)
+% SINGLE_RUN computes Longstaff-Schwartz result for a single run
+
+    %
+    % Phase 1: least squares regression
+    %
+    dW = sqrt(dt)*randn(N,M);                              % Brownian increments
+    S  = cumprod([repmat(S0,1,M); exp((r-sig^2/2)*dt+sig*dW)]);  % paths
+
+    P = max(K-S(end,:),0);  % put option payoff
+
+    % loop backwards in time
+
+    for n = N:-1:2
+        P    = exp(-r*dt)*P;                            % discount 
+        ind  = 1:M;                                     % use all paths
+        X    = S(n,ind)';
+        Y    = P(ind)';
+        A    = [ ones(size(X)) (1-X) 1/2*(2-4*X-X.^2)]; % 3 basis functions
+        beta = A\Y;                                     % linear regression
+        betas(n,:) = beta';
+        C    = A*beta;                                  % continuation value
+
+        ind  = ind(K-X > C);
+        P(ind) = K-S(n,ind);
+    end
+
+    P   = exp(-r*dt)*P;
+    val = sum(P)/M;
+    sd  = sqrt((sum(P.^2)/M - val^2)/M);
+
+    fprintf(' Phase 1: option value = %f, std dev = %f \n',val, sd);
+
+    %
+    % Phase 2: final price evaluation
+    %
+    % second set of paths for price evaluation
+    dW = sqrt(dt)*randn(N,M);                              % Brownian increments
+    S  = cumprod([repmat(S0,1,M); exp((r-sig^2/2)*dt+sig*dW)]);  % paths
+
+    P = max(K-S(end,:),0);  % put option payoff
+
+    % loop backwards in time
+    for n = N:-1:2
+        P    = exp(-r*dt)*P;                            % discount 
+        ind  = 1:M;                                     % use all paths
+        X    = S(n,ind)';
+        A    = [ ones(size(X)) (1-X) 1/2*(2-4*X-X.^2)]; % 3 basis functions
+        beta = betas(n,:)';
+        C    = A*beta;                                  % continuation value
+
+        ind  = ind(K-X > C);
+        P(ind) = K-S(n,ind);
+    end
+
+    P   = exp(-r*dt)*P;
+    val = sum(P)/M;
+    sd  = sqrt((sum(P.^2)/M - val^2)/M);
+
+    fprintf(' Phase 2: option value = %f, std dev = %f \n',val, sd);
+end
